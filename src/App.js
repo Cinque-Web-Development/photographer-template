85cbade1bb962797b9cecff27ec5f0cb4b94c1ce
Samuel Trahan <samtrahan91@gmail.com>
2021-03-02 16:05:29 -0600

    porfolio component added

--- src/App.js
+++ src/App.js
@@ -4,13 +4,21 @@ import {BrowserRouter as Router, Route} from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar';
 import Feature from './components/Feature/Feature';
 import Footer from './components/Footer/Footer';
+import Portfolio from './components/Portfolio/Portfolio';
 
 function App() {
   return (
     <Router>
     <div className="App">
       <NavBar />
-      <Feature />
+      <Route exact path="/" render={() => 
+        <Feature />
+      }>
+      </Route>
+      <Route exact path="/portfolio" render={() => 
+        <Portfolio />
+      }>
+      </Route>
       <Footer />
     </div>
     </Router>
